994fbcaa2e60b382317de7a2c41a741c87037808
bishwashere <b.bishwas@gmail.com>
2019-06-29 19:10:18 +0530

    Rainy season: Diet and mental health

---
+++ entrepreneurship/generate_page.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+    Usage:
+
+    python generate_page.py > ./post0629.html
+
+"""
+
+from jinja2 import Environment, FileSystemLoader
+
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+template = env.get_template('child.html')
+output = template.render()
+print(output)
